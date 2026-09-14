72bc593a88daaa28358c8b89604111613d38a208
TrifingZW <TrifingZW@gmail.com>
2026-09-14 21:11:50 +0800

    rebrand: Zed Vela

--- crates/release_channel/src/lib.rs
+++ crates/release_channel/src/lib.rs
@@ -44,10 +44,10 @@ pub static RELEASE_CHANNEL: LazyLock<ReleaseChannel> =
 #[cfg(target_os = "windows")]
 pub fn app_identifier() -> &'static str {
     match *RELEASE_CHANNEL {
-        ReleaseChannel::Dev => "Zed-Editor-Dev",
-        ReleaseChannel::Nightly => "Zed-Editor-Nightly",
-        ReleaseChannel::Preview => "Zed-Editor-Preview",
-        ReleaseChannel::Stable => "Zed-Editor-Stable",
+        ReleaseChannel::Dev => "Zed-Vela-Dev",
+        ReleaseChannel::Nightly => "Zed-Vela-Nightly",
+        ReleaseChannel::Preview => "Zed-Vela-Preview",
+        ReleaseChannel::Stable => "Zed-Vela-Stable",
     }
 }
 
@@ -205,10 +205,10 @@ impl ReleaseChannel {
     /// Returns the display name for this [`ReleaseChannel`].
     pub fn display_name(&self) -> &'static str {
         match self {
-            ReleaseChannel::Dev => "Zed Dev",
-            ReleaseChannel::Nightly => "Zed Nightly",
-            ReleaseChannel::Preview => "Zed Preview",
-            ReleaseChannel::Stable => "Zed",
+            ReleaseChannel::Dev => "Zed Vela",
+            ReleaseChannel::Nightly => "Zed Vela Nightly",
+            ReleaseChannel::Preview => "Zed Vela Preview",
+            ReleaseChannel::Stable => "Zed Vela",
         }
     }
 
@@ -227,10 +227,10 @@ impl ReleaseChannel {
     /// This also has to match the bundle identifier for Zed on macOS.
     pub fn app_id(&self) -> &'static str {
         match self {
-            ReleaseChannel::Dev => "dev.zed.Zed-Dev",
-            ReleaseChannel::Nightly => "dev.zed.Zed-Nightly",
-            ReleaseChannel::Preview => "dev.zed.Zed-Preview",
-            ReleaseChannel::Stable => "dev.zed.Zed",
+            ReleaseChannel::Dev => "dev.zed-vela.Zed-Vela-Dev",
+            ReleaseChannel::Nightly => "dev.zed-vela.Zed-Vela-Nightly",
+            ReleaseChannel::Preview => "dev.zed-vela.Zed-Vela-Preview",
+            ReleaseChannel::Stable => "dev.zed-vela.Zed-Vela",
         }
     }
 
